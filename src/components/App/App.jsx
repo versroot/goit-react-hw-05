7179249a3b8cf4240ae30aaaec60f78d4a3bdb8b
versroot <131954932+versroot@users.noreply.github.com>
2025-04-12 12:34:27 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,15 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 const Navigation = lazy(() => import("../Navigation/Navigation.jsx"));
-const HomePage = lazy(() => import("../../Pages/HomePage/HomePage.jsx"));
-const MoviesPage = lazy(() => import("../../Pages/MoviesPage/MoviesPage.jsx"));
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage.jsx"));
 const MovieDetailsPage = lazy(() =>
-  import("../../Pages/MovieDetailsPage/MovieDetailsPage.jsx")
+  import("../../pages/MovieDetailsPage/MovieDetailsPage.jsx")
 );
 const Cast = lazy(() => import("../Cast/Cast.jsx"));
 const Reviews = lazy(() => import("../Reviews/Reviews.jsx"));
 const NotFoundPage = lazy(() =>
-  import("../../Pages/NotFoundPage/NotFoundPage.jsx")
+  import("../../pages/NotFoundPage/NotFoundPage.jsx")
 );
 
 export default function App() {
